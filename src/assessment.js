cd088343dcdea9c3144d799ca8b63a41169d38e5
raleigh wayland <rtwayland@gmail.com>
2017-01-20 14:26:16 -0700

    complete

--- src/assessment.js
+++ src/assessment.js
@@ -26,19 +26,19 @@
 // Remove entries from the array until only correct answers remain
 
 // Which function(s) access the playball variable and get homerun (Delete wrong answers, leave correct ones)
-var scopeArray1 = ["grandpa", "dad", "son", "uncle", "cousin"];
+var scopeArray1 = ["grandpa"];
 
 // Which function(s) access the playball variable and get touchdown (Delete wrong answers, leave correct ones)
-var scopeArray2 = ["grandpa", "dad", "son", "uncle", "cousin"];
+var scopeArray2 = ["dad"];
 
 // Which function(s) access the badjoke variable and get undefined (Delete wrong answers, leave correct ones)
-var scopeArray3 = ["grandpa", "dad", "son", "uncle", "cousin"];
+var scopeArray3 = ["grandpa", "dad", "son"];
 
 // Which functions accesss the badjoke variable and get "Har" (Delete wrong answers, leave correct ones)
-var scopeArray4 = ["grandpa", "dad", "son", "uncle", "cousin"];
+var scopeArray4 = ["uncle", "cousin"];
 
 // Which functions access the playnintendo variable and get Mario (Delete wrong answers, leave correct ones)
-var scopeArray5 = ["grandpa", "dad", "son", "uncle", "cousin"];
+var scopeArray5 = ["son"];
 
 
 // #2  ###################
@@ -50,14 +50,25 @@ var foo;
   which changes the variable foo (above) to 'bar'
   And resolve the promise when setTimeout completes.
 */
+function async() {
+    var defer = $q.defer();
 
+    setTimeout(function() {
+        foo = 'bar';
+        defer.resolve(foo);
+    }, 2000);
+
+    return defer.promise;
+}
 
 // #3  ###################
 // # Context 1
 // Write a function called context1 that takes in 4 parameters: A function called myFn, an object called context, param1, and param2.
 // Invoke myFn explicitly setting the context to the object called context.  Pass in param1 and param2 in order as well.
 
-
+function context1(myFn, context, param1, param2) {
+    return myFn.call(context, param1, param2);
+}
 
 
 // #4  ###################
@@ -65,7 +76,9 @@ var foo;
 // Write a function called context2 that takes in 3 parameters: A function called myFn, an object called context, and an array called params
 // Invoke myFn explicitly setting the context to the object called context.  Pass in params
 
-
+function context2(myFn, context, params) {
+    return myFn.apply(context, params);
+}
 
 
 // #5  ###################
@@ -73,34 +86,59 @@ var foo;
 // Write a function called context3 that takes in 2 parameters: A function called myFn, and an object called context
 // Make sure the function is permanently linked to the context.  This should give you a new function, return it.
 
-
+function context3(myFn, context) {
+    var bound = myFn.bind(context);
+    return bound;
+}
 
 
 // #6  ###################
 // # Constructor Function
 // Make a constructor function called taco that takes in 3 parameters: shell, meat, veggies and assigns them to identically named properties.
 
-
+function Taco(shell, meat, veggies) {
+    this.shell = shell;
+    this.meat = meat;
+    this.veggies = veggies;
+}
 
 
 // #7  ###################
 // # Implicit binding
 // Make a constructor function called burrito.  It has a property called percentLeft = 100.  It has a property called eat that is a function.  When eat is invoked it uses context to implicitly subract 25 from the percentLeft on the burrito.
 
-
+function Burrito() {
+    this.percentLeft = 100;
+    this.eat = function() {
+        this.percentLeft -= 25;
+    }
+}
 
 // #8  ###################
 // # Prototype 1
 // Add prototype function to the array type that doubles the value of every item in the array
 
+Array.prototype.doubler = function() {
+    var newArray = this;
+    for (var i = 0; i < newArray.length; i++) {
+        newArray[i] *= 2;
+    }
 
+    return newArray;
+};
 
 
 // #9  ###################
 // # Prototype 2
 // Write a constructor function called chimichanga.  It has a property called percentLeft = 100.  It has a prototype function called eat.  When eat is invoked it uses context to implicitly subract 20 from the precentLeft on the chimichanga.
 
+function Chimichanga() {
+    this.percentLeft = 100;
+}
 
+Chimichanga.prototype.eat = function() {
+    this.percentLeft -= 20;
+};
 
 
 // #10  ###################
@@ -108,7 +146,11 @@ var foo;
 // Write a function called sentence machine.  It takes in a parameter called partOne.  It returns a function called sentenceSmasher.
 // When sentenceSmasher is invoked it should take in a parameter called partTwo and return a new string that adds partOne and partTwo together.
 
-
+function sentenceMachine(partOne) {
+    return function(partTwo) {
+        return partOne + partTwo;
+    };
+}
 
 // #11  ###################
 // # Closure 2
@@ -123,7 +165,16 @@ var foo;
 // }
 // ```
 
-
+function subway(personName) {
+    var order = {
+        orderPerson: personName,
+        ingredients: []
+    }
+    return function(ingredient) {
+        order.ingredients.push(ingredient);
+        return order;
+    }
+}
 
 // #12  ###################
 // # Type checking
@@ -131,3 +182,13 @@ var foo;
 // If both parameters are the same type an the same value return "Exact match".
 // If both parameters have the same value but are different types return "Different types"
 // Otherwise return "Different values"
+
+function compareValues(val1, val2) {
+    if (val1 === val2) {
+        return 'Exact match';
+    } else if (val1 == val2) {
+        return 'Different types';
+    } else {
+        return 'Different values';
+    }
+}
